794d75ec0c2d44a583f688eabd5df19c3b5bf056
Frédéric Goualard <Frederic.Goualard@univ-nantes.fr>
2024-07-12 17:47:33 +0200

    Changed tests to remove the use of (discontinued?) @includetests macro

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,12 +26,70 @@ using MicroFloatingPoints.MFPUtils
 using MicroFloatingPoints.MFPRandom
 using MicroFloatingPoints.MFPPlot
 
-# Call "julia runtests.jl [tests1] [tests2] ..."
-# to launch only the tests in `test1.jl`, `test2.jl`, ...
 # Calling "julia runtests.jl" launches all tests in the directory.
 @testset ExtendedTestSet "All the tests" begin
-    @testset "All tests" begin
-        @includetests ARGS
+    @testset "Arithmetic tests" begin
+        include("arithmetic.jl")
+    end
+    @testset "Constructor tests" begin
+        include("constructors.jl")
+    end
+    @testset "Emask tests" begin
+        include("Emask.jl")
+    end
+    @testset "fmask tests" begin
+        include("fmask.jl")
+    end
+    @testset "prevnext tests" begin
+        include("prevnext.jl")
+    end
+    @testset "smask tests" begin
+        include("smask.jl")
+    end
+    @testset "bias tests" begin
+        include("bias.jl")
+    end
+    @testset "convert tests" begin
+        include("convert.jl")
+    end
+    @testset "Emax tests" begin
+        include("Emax.jl")
+    end
+    @testset "iterator tests" begin
+        include("iterator.jl")
+    end
+    @testset "utils tests" begin
+        include("utils.jl")
+    end
+    @testset "classes tests" begin
+        include("classes.jl")
+    end
+    @testset "decompose tests" begin
+        include("decompose.jl")
+    end
+    @testset "Emin tests" begin
+        include("Emin.jl")
+    end
+    @testset "misc tests" begin
+        include("misc.jl")
+    end
+    @testset "signbit tests" begin
+        include("signbit.jl")
+    end
+    @testset "constants tests" begin
+        include("constants.jl")
+    end
+    @testset "display tests" begin
+        include("display.jl")
+    end
+    @testset "error tests" begin
+        include("error.jl")
+    end
+    @testset "parse tests" begin
+        include("parse.jl")
+    end
+    @testset "sign tests" begin
+        include("sign.jl")
     end
 end;
 
